ebf91fda09dc33f8be000bac3380122b3221619f
Erna23 <trueyn03@gmail.com>
2025-07-02 22:43:08 +0900

    Add: 개발환경 세팅

---
+++ app/src/main/java/com/example/commit/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.commit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.commit.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+
+        val startFragmentId = intent.getIntExtra("start_fragment", R.id.nav_home)
+        bottomNavigationView.selectedItemId = startFragmentId
+
+    }
+
+    private fun initBottomNavigation() {
+
+        bottomNavigationView = binding.BottomNavi
+
+        bottomNavigationView.selectedItemId = R.id.nav_home
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.NavFrame.id, FragmentHome()) // Replace with your fragment
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_bookmark -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.NavFrame.id, FragmentBookmark())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_request -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.NavFrame.id, FragmentRequest())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_chat -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.NavFrame.id, FragmentChat())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_mypage -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.NavFrame.id, FragmentMypage())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+    }
+}
